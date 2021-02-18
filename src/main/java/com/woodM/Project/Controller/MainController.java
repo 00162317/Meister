85284509a20212e4926866863a1b315e590e68bb
David Guardado <00184217@uca.edu.sv>
2021-02-18 17:52:39 -0600

    htmls

--- src/main/java/com/woodM/Project/Controller/MainController.java
+++ src/main/java/com/woodM/Project/Controller/MainController.java
@@ -11,7 +11,55 @@ public class MainController {
 	@RequestMapping("/index")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("index");
+		mav.setViewName("index1");
+		return mav;
+	}
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("about");
+		return mav;
+	}
+	@RequestMapping("/agregar")
+	public ModelAndView agregar() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("agregar");
+		return mav;
+	}
+	@RequestMapping("/eliminar")
+	public ModelAndView eliminar() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("eliminar");
+		return mav;
+	}
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		return mav;
+	}
+	@RequestMapping("/modificar")
+	public ModelAndView modificar() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("modificar");
+		return mav;
+	}
+	@RequestMapping("/product")
+	public ModelAndView product() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("product");
+		return mav;
+	}
+	@RequestMapping("/register")
+	public ModelAndView register() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("register");
+		return mav;
+	}
+	@RequestMapping("/registro")
+	public ModelAndView registro() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("registro");
 		return mav;
 	}
 }
